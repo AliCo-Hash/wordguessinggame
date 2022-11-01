90e74472bb7fa46e80f8ecc452618e8153b2bd34
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-11-01 19:45:19 +0000

    quick refactoring of tests to make sure they still pass

--- app/src/test/java/game/GameTest.java
+++ app/src/test/java/game/GameTest.java
@@ -5,19 +5,19 @@ import static org.junit.Assert.*;
 
 public class GameTest {
   @Test public void testGetsWordToGuess() {
-    Game game = new Game("B_____");
+    Game game = new Game();
     String word = "B_____";
     assertEquals(game.getWordToGuess(word), word);
   }
 
   @Test public void testGetsWordToGuess2() {
-    Game game = new Game("MAKERS");
+    Game game = new Game();
     String word = "MAKERS";
     assertEquals(game.getWordToGuess(word), "M_____");
   }
   
   @Test public void getRemainingAttempts() {
-    Game game = new Game("MAKERS");
+    Game game = new Game();
     assertEquals(game.attempts, Integer.valueOf(10));
   }
 }
